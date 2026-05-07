d62a045480cf200916d8933e9a587682124ebc0e
Tamoehrchen <gilgenast24175@hs-ansbach.de>
2026-05-07 12:33:14 +0200

    Bonusaufgabe 3 Shy Button

---
+++ UebungShyButton/script.js
@@ -0,0 +1,35 @@
+
+const btn = document.getElementById("button");
+
+btn.addEventListener("mouseover", function () {
+
+
+    btn.style.transition = "transform 0.15s ease";
+    btn.style.transform = "scale(0.7)";
+
+    setTimeout(function () {
+
+        const x = (Math.random()-0.5) *800;
+        const y = (Math.random()-0.5) *500;
+
+        btn.style.transition = "transform 0.7s cubic-bezier(0.175, 0.885, 0.32, 1.275)";
+
+        btn.style.transform = "translate(" + x + "px, " + y + "px) scale(1)";
+
+        btn.style.backgroundColor = "rgb(243, 105, 105)";
+        btn.style.borderColor = "rgb(245, 177, 177)";
+
+    }, 120);
+});
+
+btn.addEventListener("mouseleave", function () {
+
+    setTimeout(function () {
+        btn.style.backgroundColor = "";
+        btn.style.borderColor = "";
+    }, 500);
+});
+
+btn.addEventListener("click", function () {
+    alert('You Got Me!');
+});
